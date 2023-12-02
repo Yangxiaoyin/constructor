b3bdeec257544db219f03fe74cd61952358bfb47
Chen Yang <chenyang@chens-air.home>
2023-12-01 20:21:59 -0500

    pure virtual function

--- Constructor/Entity_virtual.cpp
+++ Constructor/Entity_virtual.cpp
@@ -1,24 +1,34 @@
 #include<iostream>
 #include<string>
 
-class Entity{
-    public:
-       virtual std::string GetName(){return "entity";}
+class Printable
+{
+    virtual std::string GetName2()=0; //force to have a implementation
+
 };
 
-class Player: public Entity{
+class Entity
+{
+    public:
+       virtual std::string GetName(){return "entity";} //store a vTable to dispatch the relation
+       std::string GetName2(){return "Entity";} //force to have a implementation
+};
+
+class Player: public Entity
+{
     private:
       std::string m_name;
     public:
       Player(const std::string& name):m_name(name){}
       std::string GetName() override {return m_name;}
+      std::string GetName2()  {return m_name;}
 };
 
 int main(){
     Entity *e=new Entity();
     std::cout<<e->GetName()<<std::endl;
     Player *p = new Player("cherno2");
-    std::cout<<p->GetName()<<std::endl;
+    std::cout<<p->GetName2()<<std::endl;
     //std::cout<<"Hello world"<<std::endl;
     std::cin.get();
 }
